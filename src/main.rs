50419ae5dc93938fd50f68849525995a67199f59
Eric Culp <eculperic@gmail.com>
2019-08-19 10:04:29 -0700

    Edit comments with a thankyou message after being fixed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate comrak;
+extern crate failure;
 extern crate git_version;
 extern crate htmlescape;
 extern crate log;
@@ -6,7 +7,7 @@ extern crate orca;
 extern crate simple_logger;
 
 use std::collections::VecDeque;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 const DRY_RUN: bool = false;
 
@@ -57,11 +58,13 @@ fn get_pass(p: &str) -> String {
 }
 
 fn strip_type(s: &str) -> &str {
-    if s.starts_with("t3_") {
-        &s[3..]
-    } else {
-        s
+    let b = s.as_bytes();
+    if let (Some(b't'), Some(d), Some(b'_')) = (b.get(0), b.get(1), b.get(2)) {
+        if d.is_ascii_digit() {
+            return &s[3..];
+        }
     }
+    s
 }
 
 fn find_comment<'a>(
@@ -82,32 +85,111 @@ fn find_comment<'a>(
     None
 }
 
+struct Backoff {
+    fails: u64,
+}
+
+impl Backoff {
+    fn ok(&mut self) {
+        if self.fails > 0 {
+            self.fails -= 1;
+        }
+    }
+
+    fn fail_wait(&mut self) {
+        use rand::Rng;
+        std::thread::sleep(Duration::from_millis(
+            1000 * self.fails + rand::thread_rng().gen_range(0, 1000),
+        ));
+        self.fails = (self.fails + 1).min(10);
+    }
+}
+
+struct MadeComment {
+    parent_name: String,
+    name: String,
+    link_id: String,
+    edited: bool,
+}
+
+fn write_reply(out: &mut String, comment: &orca::data::Comment) {
+    use std::fmt::Write;
+    write!(
+        out,
+        "Your comment uses fenced code blocks (e.g. blocks surrounded \n\
+         with ```` ``` ````). These don't render correctly in old \n\
+         reddit even if you authored them in new reddit. Please use \n\
+         code blocks indented with 4 spaces instead. See what the \n\
+         comment looks like in \n\
+         [new](https://new.reddit.com{permalink}) \n\
+         and \n\
+         [old](https://old.reddit.com{permalink}) \n\
+         reddit. \n\
+         [My page](https://github.com/singron/old-reddit-fmt-bot/blob/master/about.md) \n\
+         has easy ways to indent code as well as information and source code for this bot.",
+        permalink = EscapeMarkdownLink(&comment.permalink),
+    )
+    .unwrap();
+}
+
 struct MultiSubreddit<'a> {
     app: &'a orca::App,
+    username: &'a str,
     names: &'static [&'static str],
     caches: Vec<VecDeque<orca::data::Comment>>,
     last_comment_names: Vec<Option<String>>,
+    comments_made: Vec<MadeComment>,
+    comments_made_dirty: bool,
+    last_comments_made_check: Option<Instant>,
+    backoff: Backoff,
 }
 
 impl<'a> MultiSubreddit<'a> {
-    fn new(app: &'a orca::App, names: &'static [&'static str]) -> MultiSubreddit<'a> {
+    fn new(
+        app: &'a orca::App,
+        username: &'a str,
+        names: &'static [&'static str],
+    ) -> MultiSubreddit<'a> {
         MultiSubreddit {
             app,
+            username,
             names,
             caches: vec![VecDeque::new(); names.len()],
             last_comment_names: vec![None; names.len()],
+            comments_made: Vec::new(),
+            comments_made_dirty: true,
+            last_comments_made_check: None,
+            backoff: Backoff { fails: 0 },
         }
     }
+
+    fn load_comments_made(&mut self) -> Result<(), failure::Error> {
+        let mut opts = orca::app::UserListingOpts::default();
+        opts.limit(100);
+        let comments: orca::data::Listing<orca::data::Comment> =
+            self.app.get_user_comments(self.username, &opts)?;
+        let mut comments_made = Vec::with_capacity(comments.children.len());
+        for comment in comments.children {
+            let comment: orca::data::Comment = comment;
+            comments_made.push(MadeComment {
+                parent_name: comment.parent_id,
+                name: comment.name,
+                edited: comment.body.contains("EDIT:"),
+                link_id: comment.link_id,
+            });
+        }
+        self.comments_made = comments_made;
+        self.comments_made_dirty = false;
+        Ok(())
+    }
+
     fn refresh(&mut self) {
-        let mut fails = 0;
         for (idx, subreddit) in self.names.iter().enumerate() {
             let last = self.last_comment_names[idx].as_ref().map(|s| s.as_str());
             let res: orca::data::Listing<orca::data::Comment> = loop {
                 match self.app.get_recent_comments(subreddit, Some(100), last) {
                     Ok(x) => {
-                        if fails > 0 {
-                            fails = fails - 1;
-                        }
+                        self.backoff.ok();
                         break x;
                     }
                     Err(e) => {
@@ -115,11 +197,7 @@ impl<'a> MultiSubreddit<'a> {
                             "Error get_recent_comments({:?}, {:?}): {}",
                             subreddit, last, e
                         );
-                        use rand::Rng;
-                        std::thread::sleep(Duration::from_millis(
-                            1000 * fails + rand::thread_rng().gen_range(0, 1000),
-                        ));
-                        fails = (fails + 1).min(10);
+                        self.backoff.fail_wait();
                     }
                 }
             };
@@ -129,6 +207,177 @@ impl<'a> MultiSubreddit<'a> {
             // get_recent_comments returns reverse-chronological order, so unreverse it.
             self.caches[idx].extend(res.children.into_iter().rev());
         }
+        if self.comments_made_dirty {
+            loop {
+                match self.load_comments_made() {
+                    Ok(x) => {
+                        self.backoff.ok();
+                        break x;
+                    }
+                    Err(e) => {
+                        println!("Error load_comments_made: {}", e);
+                        self.backoff.fail_wait();
+                    }
+                }
+            }
+        }
+    }
+
+    fn on_new_comment(&mut self, comment: orca::data::Comment) {
+        use std::convert::TryFrom;
+        let created = std::time::SystemTime::UNIX_EPOCH
+            .checked_add(std::time::Duration::from_secs(
+                u64::try_from(comment.created_utc as i64).unwrap(),
+            ))
+            .unwrap();
+        let age = created.elapsed().unwrap();
+        println!("https://www.reddit.com{} {:?}", comment.permalink, age);
+        // Never reply to ourselves
+        if comment.author == self.username {
+            return;
+        }
+        // Reddit responses are entity encoded for legacy reasons unless the client passes
+        // raw_json=1, which orca doesn't.
+        let body = match htmlescape::decode_html(&comment.body) {
+            Err(e) => {
+                println!("Error: {:?}", e);
+                return;
+            }
+            Ok(x) => x,
+        };
+        if !contains_fenced_block(&body) {
+            return;
+        }
+        // This comment from the comments stream doesn't include replies, so let's load the
+        // whole tree.
+        let tree = loop {
+            match self.app.get_comment_tree(strip_type(&comment.link_id)) {
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    return;
+                }
+                Ok(x) => break x,
+            }
+        };
+        let tree_comment = find_comment(&tree, &comment.id);
+        if let Some(tree_comment) = tree_comment {
+            let mut already_replied = false;
+            for reply in &tree_comment.replies.children {
+                if reply.author == self.username {
+                    already_replied = true;
+                    break;
+                }
+            }
+            // Don't reply to the same comment again.
+            if already_replied {
+                return;
+            }
+        } else {
+            // Maybe it was deleted?
+            println!(
+                "Could not find comment {} on link {}",
+                comment.id, comment.link_id
+            );
+            return;
+        }
+        let max_age = std::time::Duration::from_secs(60 * 60 * 24); // 24h
+        if age > max_age {
+            return;
+        }
+        println!("{}", body);
+        let mut reply = String::new();
+        write_reply(&mut reply, &comment);
+        println!("{}", &reply);
+        if DRY_RUN {
+            println!("DRY_RUN: not commenting");
+            return;
+        }
+        self.comments_made_dirty = true;
+        if let Err(e) = self.app.comment(&reply, &comment.name) {
+            println!("Error in comment: {}", e);
+        }
+    }
+
+    fn check_comments_made(&mut self) {
+        for comment_made in &mut self.comments_made {
+            if comment_made.edited {
+                continue;
+            }
+            let tree = match self.app.get_comment_tree(strip_type(&comment_made.link_id)) {
+                Err(e) => {
+                    println!(
+                        "Error in get_comment_tree({:?}): {}",
+                        comment_made.link_id, e
+                    );
+                    self.backoff.fail_wait();
+                    continue;
+                }
+                Ok(x) => {
+                    self.backoff.ok();
+                    x
+                }
+            };
+            let parent_comment = match find_comment(&tree, strip_type(&comment_made.parent_name)) {
+                Some(x) => x,
+                None => {
+                    println!(
+                        "Could not find comment {} in {}",
+                        comment_made.parent_name, comment_made.link_id
+                    );
+                    continue;
+                }
+            };
+            if contains_fenced_block(&parent_comment.body) {
+                continue;
+            }
+            // They fixed their comment
+            println!(
+                "Should edit reply to https://www.reddit.com{}",
+                parent_comment.permalink
+            );
+            let mut new_reply = "EDIT: Thanks for editing your comment!\n\n".to_string();
+            write_reply(&mut new_reply, &parent_comment);
+            println!("{}", new_reply);
+            if DRY_RUN {
+                println!("DRY_RUN: not editing")
+            } else {
+                if let Err(e) = self.app.edit(&new_reply, &comment_made.name) {
+                    println!("Error in edit({:?}): {}", &comment_made.name, e);
+                }
+            }
+            comment_made.edited = true;
+        }
+        self.last_comments_made_check = Some(Instant::now());
+    }
+
+    fn process(&mut self) {
+        loop {
+            self.refresh();
+            loop {
+                let mut created_utc = None;
+                let mut min_idx = None;
+                for (idx, cache) in self.caches.iter().enumerate() {
+                    if let Some(c) = cache.front() {
+                        if created_utc.is_none() || c.created_utc < created_utc.unwrap() {
+                            min_idx = Some(idx);
+                            created_utc = Some(c.created_utc);
+                        }
+                    }
+                }
+                if let Some(min_idx) = min_idx {
+                    let comment = self.caches[min_idx].pop_front().unwrap();
+                    self.on_new_comment(comment);
+                }
+                break;
+            }
+            if self
+                .last_comments_made_check
+                .map(|i| i < Instant::now() - Duration::from_secs(5 * 60))
+                .unwrap_or(true)
+            {
+                self.check_comments_made();
+            }
+        }
     }
 }
 
@@ -136,19 +385,6 @@ impl Iterator for MultiSubreddit<'_> {
     type Item = orca::data::Comment;
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            let mut created_utc = None;
-            let mut min_idx = None;
-            for (idx, cache) in self.caches.iter().enumerate() {
-                if let Some(c) = cache.front() {
-                    if created_utc.is_none() || c.created_utc < created_utc.unwrap() {
-                        min_idx = Some(idx);
-                        created_utc = Some(c.created_utc);
-                    }
-                }
-            }
-            if let Some(min_idx) = min_idx {
-                return self.caches[min_idx].pop_front();
-            }
             // We are empty
             self.refresh();
         }
@@ -191,98 +427,6 @@ impl std::fmt::Display for EscapeMarkdownLink<'_> {
     }
 }
 
-fn process_comments(
-    username: &str,
-    max_age: Duration,
-    app: &orca::App,
-    comments: &mut MultiSubreddit,
-) {
-    for comment in comments {
-        use std::convert::TryFrom;
-        let created = std::time::SystemTime::UNIX_EPOCH
-            .checked_add(std::time::Duration::from_secs(
-                u64::try_from(comment.created_utc as i64).unwrap(),
-            ))
-            .unwrap();
-        let age = created.elapsed().unwrap();
-        println!("https://www.reddit.com{} {:?}", comment.permalink, age);
-        // Never reply to ourselves
-        if comment.author == username {
-            continue;
-        }
-        // Reddit responses are entity encoded for legacy reasons unless the client passes
-        // raw_json=1, which orca doesn't.
-        let body = match htmlescape::decode_html(&comment.body) {
-            Err(e) => {
-                println!("Error: {:?}", e);
-                continue;
-            }
-            Ok(x) => x,
-        };
-        if !contains_fenced_block(&body) {
-            continue;
-        }
-        // This comment from the comments stream doesn't include replies, so let's load the
-        // whole tree.
-        let tree = loop {
-            match app.get_comment_tree(strip_type(&comment.link_id)) {
-                Err(e) => {
-                    println!("Error: {:?}", e);
-                    continue;
-                }
-                Ok(x) => break x,
-            }
-        };
-        let tree_comment = find_comment(&tree, &comment.id);
-        if let Some(tree_comment) = tree_comment {
-            let mut already_replied = false;
-            for reply in &tree_comment.replies.children {
-                if reply.author == username {
-                    already_replied = true;
-                    break;
-                }
-            }
-            // Don't reply to the same comment again.
-            if already_replied {
-                continue;
-            }
-        } else {
-            // Maybe it was deleted?
-            println!(
-                "Could not find comment {} on link {}",
-                comment.id, comment.link_id
-            );
-            continue;
-        }
-        if age > max_age {
-            continue;
-        }
-        println!("{}", body);
-        let reply = format!(
-            "Your comment uses fenced code blocks (e.g. blocks surrounded \n\
-             with ```` ``` ````). These don't render correctly in old \n\
-             reddit even if you authored them in new reddit. Please use \n\
-             code blocks indented with 4 spaces instead. See what the \n\
-             comment looks like in \n\
-             [new](https://new.reddit.com{permalink}) \n\
-             and \n\
-             [old](https://old.reddit.com{permalink}) \n\
-             reddit. \n\
-             [My page](https://github.com/singron/old-reddit-fmt-bot/blob/master/about.md) \n\
-             has easy ways to indent code as well as information and source code for this bot.",
-            permalink = EscapeMarkdownLink(&comment.permalink),
-        );
-        println!("{}", &reply);
-        if DRY_RUN {
-            println!("DRY_RUN: not commenting");
-            continue;
-        }
-        if let Err(e) = app.comment(&reply, &comment.name) {
-            println!("Error in comment: {}", e);
-        }
-    }
-}
-
 fn main() {
     simple_logger::init_with_level(log::Level::Warn).unwrap();
     let secret = get_pass("Reddit/old-reddit-fmt-bot/secret");
@@ -297,9 +441,17 @@ fn main() {
     drop(id);
     drop(password);
 
-    let max_age = std::time::Duration::from_secs(60 * 60 * 24); // 24h
-    let mut multi = MultiSubreddit::new(&app, &["programming", "rust", "NixOS"]);
-    process_comments(username, max_age, &app, &mut multi);
+    let mut multi = MultiSubreddit::new(&app, username, &["programming", "rust", "NixOS"]);
+    loop {
+        match multi.load_comments_made() {
+            Ok(_) => break,
+            Err(e) => {
+                println!("Error in load_comments_made: {}", e);
+                continue;
+            }
+        }
+    }
+    multi.process();
 }
 
 #[cfg(test)]
